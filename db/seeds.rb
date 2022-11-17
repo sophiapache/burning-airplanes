6234a492ded21a31d50be3eac6aa28e182c43d77
Sophia Pache <sophpache@gmail.com>
2022-11-17 11:59:05 +1100

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,14 @@ p1 = Airplane.create :columns => '6', :rows => '15', :name => 'AX3'
 Flight.destroy_all
 f1 = Flight.create :origin => 'Sydney', :destination => 'Melbourne', :date => '31/10/2022', :airplane_id => p1[:id], :flight_number => 33
 f2 = Flight.create :origin => 'Melbourne', :destination => 'Sydney', :date => '05/11/2022', :airplane_id => p1[:id], :flight_number => 34
+f3 = Flight.create :origin => 'Sydney', :destination => 'Melbourne', :date => '01/11/2022', :airplane_id => p1[:id], :flight_number => 35
+f4 = Flight.create :origin => 'Sydney', :destination => 'Melbourne', :date => '02/11/2022', :airplane_id => p1[:id], :flight_number => 36
+f5 = Flight.create :origin => 'Sydney', :destination => 'Melbourne', :date => '03/11/2022', :airplane_id => p1[:id], :flight_number => 37
+f6 = Flight.create :origin => 'Sydney', :destination => 'Melbourne', :date => '04/11/2022', :airplane_id => p1[:id], :flight_number => 38
+f7 = Flight.create :origin => 'Sydney', :destination => 'Melbourne', :date => '05/11/2022', :airplane_id => p1[:id], :flight_number => 39
+f8 = Flight.create :origin => 'Sydney', :destination => 'Melbourne', :date => '06/11/2022', :airplane_id => p1[:id], :flight_number => 40
+f9 = Flight.create :origin => 'Sydney', :destination => 'Melbourne', :date => '07/11/2022', :airplane_id => p1[:id], :flight_number => 41
+f10 = Flight.create :origin => 'Sydney', :destination => 'Melbourne', :date => '08/11/2022', :airplane_id => p1[:id], :flight_number => 42
 
 Reservation.destroy_all
 r1 = Reservation.create :flight_id => f1[:id], :user_id => u1[:id], :seat => '34E'
